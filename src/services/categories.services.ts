b75b55f30431ab233828a0ccc1cbd58ee8915131
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-07 10:32:56 -0300

    commit: Refatorar CategoriesService para utilizar CategoriesRepository e adicionar validação de criacao

--- src/services/categories.services.ts
+++ src/services/categories.services.ts
@@ -1,12 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { CategoriesRepository } from '../database/repositories/categories.repository';
+import { CreateCategoryDTO } from '../dtos/categories.dto';
 import { Category } from '../entities/category.entity';
+import { AppError } from '../errors/app.error';
 
 export class CategoriesService {
-  async create(): Promise<Category> {
+  constructor(private categoryRepository: CategoriesRepository) {}
+
+  async create({ title, color }: CreateCategoryDTO): Promise<Category> {
+    const foundCategory = await this.categoryRepository.findByTitle(title);
+
+    if (foundCategory) {
+      throw new AppError('Category already exists.', StatusCodes.BAD_REQUEST);
+    }
+
     const category = new Category({
-      title: 'Example Category',
-      color: '#FFFFFF',
+      title,
+      color,
     });
 
-    return category;
+    const createdCategory = await this.categoryRepository.create(category);
+
+    return createdCategory;
+  }
+
+  async index(): Promise<Category[]> {
+    const categories = await this.categoryRepository.index();
+
+    return categories;
   }
 }
