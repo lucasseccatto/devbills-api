1fe7180359560e0d8d46eb31bfd45b97ec044fdd
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-07 10:19:28 -0300

    commit: adicionar configuracao do dotenv e ajustar inicializacao do express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
+import 'dotenv/config';
+
 import express, { json } from 'express';
 
+import { setupMongo } from './database';
+import { errorHandler } from './middleware/error-handler.middleware';
 import { routes } from './routes';
 
-const app = express();
+setupMongo().then(() => {
+  const app = express();
 
-const port = 3333;
+  const port = 3333;
 
-app.use(json());
-app.use(routes);
-app.listen(port, () => console.log(`🚀 App is running at port ${port}`));
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
+
+  app.listen(port, () => console.log(`🚀 App is running at port ${port}`));
+});
